65cf318d318c6e29849a02479ee3aac0e9546a3b
pbtcodelabs-pbt <pbtcodelabs@gmail.com>
2026-09-20 16:22:45 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // توجہ فرمائیں — Service Worker
 // آفلائن سپورٹ: ایپ ایک بار کھلنے کے بعد بغیر انٹرنیٹ کے بھی چلے گی۔
 
-const CACHE_NAME = 'tawajjo-farmayen-v6';
+const CACHE_NAME = 'tawajjo-farmayen-v11';
 const APP_SHELL = [
   './',
   './index.html',
